f3d51796c40b102d86f33760c3401774de8eac25
aditya606 <55527079+aditya606@users.noreply.github.com>
2019-09-22 23:02:19 +0530

    Update question5.js

--- question5.js
+++ question5.js
@@ -13,4 +13,4 @@ var arr2 = [2,5,7,8,9];
 var iterator = arr2[Symbol.iterator]();
 arr2.map (v=>{arr1=[...arr1,iterator.next().value]
 })
-console.log(arr1);
+console.log(arr1);//output- [1,3,5,6,7,2,5,7,8,9].
